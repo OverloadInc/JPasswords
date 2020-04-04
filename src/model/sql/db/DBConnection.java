49800c2f817c11c4a34a6c708af41fb4f367bd24
Juan-Alberto Hernández-Martínez <juan.alberto.hernandez.martinez@gmail.com>
2020-04-04 13:46:50 -0600

    DBConnection class refined

--- src/model/sql/db/DBConnection.java
+++ src/model/sql/db/DBConnection.java
@@ -1,46 +1,113 @@
-package model;
+package model.sql.db;
 
 import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.*;
 
 public class DBConnection {
+
     private String host;
     private String user;
     private String password;
-    private Connection conn;
-        
-    public DBConnection() {        
-    }
-    
+    private Connection connection;
+
+    public DBConnection() {
+        host = "jdbc:postgresql://localhost:5432/dbPasswords";
+        user = "postgres";
+        password = "postgres";
+    }  
+
     public Connection connect() throws SQLException {
-        try{
-            host = "jdbc:postgresql://localhost/dbBank";
-            user = "postgres";
-            password = "postgres";
-            
+        try {            
             Class.forName("org.postgresql.Driver");
-            
-            conn = DriverManager.getConnection(host, user, password);
-            
-            if(conn != null) {
+
+            connection = DriverManager.getConnection(host, user, password);
+
+            if (connection != null) {
                 System.out.println("Connected to database");
             }
             else {
                 System.out.println("Connection failed");
             }
         }
-        catch (ClassNotFoundException ex) {        
+        catch (ClassNotFoundException ex) {
             Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        return conn;
+
+        return connection;
+    }
+
+    public void disconnect() {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        }
+        catch (SQLException e) {
+        }
     }
     
+    public void finalize() {
+        try {
+            connection.close();
+            connection = null;
+        }
+        catch(SQLException e) {            
+        }        
+    }
+    
+    public synchronized boolean executeCommand(String command) throws Exception {
+        boolean result = false;
+        Statement statement = null;
+        
+        try {            
+            statement = connection.createStatement();
+            
+            result = statement.execute(command);
+        }
+        catch(SQLException e) {
+            connection.rollback();
+            throw e;
+        }
+        finally {
+            statement.close();
+            statement = null;
+        }
+        
+        return result;
+    }
+    
+    public synchronized ResultSet executeQuery(String query) throws Exception {
+        Statement statement = null;
+        ResultSet resultSet = null;
+        
+        try {
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(query);
+        }
+        catch(SQLException e) {
+            connection.rollback();
+            throw e;
+        }
+        finally {
+            if(resultSet != null) {
+                resultSet.close();
+                statement.close();
+            }
+            
+            resultSet = null;
+            statement = null;
+        }
+        
+        return resultSet;
+    }
+
     public static void main(String... args) {
         DBConnection db = new DBConnection();
         try {
             db.connect();
+            db.disconnect();
         }
         catch (SQLException ex) {
             Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
