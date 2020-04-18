bdb4d8ec6dde85c4982f3da3b991db36845fb970
Juan-Alberto Hernández-Martínez <juan.alberto.hernandez.martinez@gmail.com>
2020-04-18 14:07:46 -0500

    KindOfProduct class modified to get the existing kind of products

--- src/model/pojo/KindOfProduct.java
+++ src/model/pojo/KindOfProduct.java
@@ -1,6 +1,12 @@
 package model.pojo;
 
+import model.sql.db.DBConnection;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
 public class KindOfProduct {
+
     private int id;
     private String name;
 
@@ -18,5 +24,40 @@ public class KindOfProduct {
 
     public String getName() {
         return name;
-    }    
+    }
+
+    public boolean addKindOfProduct(KindOfProduct kindOfProduct) {
+        return false;
+    }
+
+    public ArrayList<KindOfProduct> getAllKindOfProduct() {
+        ArrayList<KindOfProduct> kindOfProducts = new ArrayList<>();
+
+        String query = "SELECT * FROM tipo_productos ORDER BY id_tipoproducto ASC;";
+
+        try {
+            DBConnection dbConnection = new DBConnection();
+            dbConnection.connect();
+
+            ResultSet resultSet = dbConnection.executeQuery(query);
+
+            while(resultSet.next()) {
+                KindOfProduct kindOfProduct = new KindOfProduct();
+                kindOfProduct.setId(resultSet.getInt("id_tipoproducto"));
+                kindOfProduct.setName(resultSet.getString("nombre"));
+
+                kindOfProducts.add(kindOfProduct);
+            }
+
+            dbConnection.disconnect();
+        }
+        catch (Exception e) {
+        }
+
+        return kindOfProducts;
+    }
+
+    public String toString() {
+        return this.name;
+    }
 }
