77738695864aabab2a3b287ce79e18b447e110f7
Juan-Alberto Hernández-Martínez <juan.alberto.hernandez.martinez@gmail.com>
2020-04-21 12:17:22 -0500

    Wrapper methods to execute SQL commands

--- src/controller/ProductKindController.java
+++ src/controller/ProductKindController.java
@@ -11,14 +11,32 @@ public class ProductKindController implements Controller {
 
     private JTextField txtProduct;
     private JButton btnAddProduct;
+    private JList existingProductsList;
 
     public ProductKindController(List components) {
         txtProduct = (JTextField)components.get(0);
         btnAddProduct = (JButton)components.get(1);
+        existingProductsList = (JList)components.get(2);
     }
 
     public ArrayList<KindOfProduct> getAllKindOfProduct() {
         return new KindOfProduct().getAllKindOfProduct();
+    }
+
+    public void addKindOfProduct() {
+        KindOfProduct kindOfProduct = new KindOfProduct();
+        kindOfProduct.setId(0);
+        kindOfProduct.setName(txtProduct.getText().trim());
+        kindOfProduct.addKindOfProduct();
+    }
+
+    public void refreshExistingProductsList() {
+        DefaultListModel nameListModel = new DefaultListModel();
+
+        for(KindOfProduct current : getAllKindOfProduct())
+            nameListModel.addElement(current);
+
+        existingProductsList.setModel(nameListModel);
     }
 
     @Override
@@ -33,10 +51,20 @@ public class ProductKindController implements Controller {
         String keySource = getKeySource(evt);
         int keyCode = evt.getKeyCode();
 
-        if(keyCode == 10 || keyCode == 38 || keyCode == 40)
+        if(keyCode == 37 || keyCode == 39)
             switch(keySource){
                 case "txtProduct": btnAddProduct.requestFocusInWindow(); break;
                 case "btnAddProduct": txtProduct.requestFocusInWindow(); selectText("txtProduct"); break;
             }
+        else if(keyCode == 10) {
+            switch(keySource){
+                case "txtProduct": btnAddProduct.requestFocusInWindow(); break;
+                case "btnAddProduct":
+                    addKindOfProduct();
+                    refreshExistingProductsList();
+                    txtProduct.requestFocusInWindow();
+                    break;
+            }
+        }
     }
 }
