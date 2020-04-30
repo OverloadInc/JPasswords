55bfa12183a583d7046931fe192947d6c6e4c459
Juan-Alberto Hernández-Martínez <juan.alberto.hernandez.martinez@gmail.com>
2020-04-30 13:40:54 -0500

    HierarchyController class added for controlling HierarchiesPanel's component actions

---
+++ src/controller/HierarchyController.java
@@ -0,0 +1,123 @@
+package controller;
+
+import model.pojo.Hierarchy;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.KeyEvent;
+import java.util.List;
+
+public class HierarchyController implements Controller {
+
+    private JTextField txtHierarchy;
+    private JButton btnHierarchy;
+    private JButton btnModifyHierarchy;
+    private JButton btnDeleteHierarchy;
+    private JTable existingHierarchiesTable;
+
+    public HierarchyController(List components) {
+        txtHierarchy = (JTextField)components.get(0);
+        btnHierarchy = (JButton)components.get(1);
+        btnModifyHierarchy = (JButton)components.get(2);
+        btnDeleteHierarchy = (JButton)components.get(3);
+        existingHierarchiesTable = (JTable)components.get(4);
+    }
+
+    public void addHierarchy() {
+        String hierarchyName = txtHierarchy.getText().trim();
+
+        if(!hierarchyName.isEmpty()) {
+            Hierarchy hierarchy = new Hierarchy();
+            hierarchy.setId(0);
+            hierarchy.setName(hierarchyName);
+
+            hierarchy.addHierarchy();
+        }
+        else
+            JOptionPane.showMessageDialog(null, "Introduce un nombre válido");
+    }
+
+    public void updateHierarchy() {
+        int row = existingHierarchiesTable.getSelectedRow();
+
+        if(row >= 0) {
+            String newHierarchyName = JOptionPane.showInputDialog("Introduce el nombre de la jerarquía deseada");
+            int newHierarchyId = Integer.parseInt(existingHierarchiesTable.getValueAt(row, 1).toString());
+
+            if(!newHierarchyName.isEmpty()) {
+                Hierarchy hierarchy = new Hierarchy();
+                hierarchy.setName(newHierarchyName);
+                hierarchy.setId(newHierarchyId);
+
+                hierarchy.updateHierarchy();
+            }
+        }
+        else
+            JOptionPane.showMessageDialog(null, "Selecciona un tipo de jerarquía");
+    }
+
+    public void deleteHierarchy() {
+        int row = existingHierarchiesTable.getSelectedRow();
+
+        if(row >= 0) {
+            String hierarchyName = existingHierarchiesTable.getValueAt(row, 0).toString();
+            int hierarchyId = Integer.parseInt(existingHierarchiesTable.getValueAt(row, 1).toString());
+
+            Hierarchy hierarchy = new Hierarchy();
+            hierarchy.setName(hierarchyName);
+            hierarchy.setId(hierarchyId);
+
+            hierarchy.deleteHierarchy();
+        }
+        else
+            JOptionPane.showMessageDialog(null, "Selecciona un tipo de jerarquía");
+    }
+
+    public void refreshExistingHierarchiesTable() {
+        DefaultTableModel tableModel = new DefaultTableModel(0, 0);
+        tableModel.setColumnIdentifiers(new String[]{"Nombre", "Tipo"});
+
+        existingHierarchiesTable.setModel(tableModel);
+
+        for(Hierarchy current : new Hierarchy().getAllHierarchies()) {
+            tableModel.addRow(new String[]{current.getName(), current.getId() + ""});
+        }
+    }
+
+    @Override
+    public void selectText(String name) {
+        switch(name) {
+            case "txtHierarchy" :
+                txtHierarchy.setSelectionStart(0);
+                txtHierarchy.setSelectionEnd(txtHierarchy.getText().length());
+                break;
+        }
+    }
+
+    @Override
+    public void setFocus(KeyEvent evt) {
+        String keySource = getKeySource(evt);
+        int keyCode = evt.getKeyCode();
+
+        if(keyCode == 37 || keyCode == 39)
+            switch(keySource){
+                case "txtHierarchy":
+                    btnHierarchy.requestFocusInWindow();
+                    break;
+                case "btnHierarchy":
+                    txtHierarchy.requestFocusInWindow();
+                    selectText("txtHierarchy");
+                    break;
+            }
+        else if(keyCode == 10) {
+            switch(keySource){
+                case "btnHierarchy":
+                    addHierarchy();
+                    refreshExistingHierarchiesTable();
+                    txtHierarchy.setText("");
+                    txtHierarchy.requestFocusInWindow();
+                    break;
+            }
+        }
+    }
+}
