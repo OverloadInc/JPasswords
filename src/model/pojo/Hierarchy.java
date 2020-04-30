8908e872a18e528b908e98bb8f2d0b955b35367b
Juan-Alberto Hernández-Martínez <juan.alberto.hernandez.martinez@gmail.com>
2020-04-30 13:39:00 -0500

    Insertion, modification and delete hierarchies operations completed

--- src/model/pojo/Hierarchy.java
+++ src/model/pojo/Hierarchy.java
@@ -1,5 +1,10 @@
 package model.pojo;
 
+import model.sql.db.DBConnection;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
 public class Hierarchy {
 
     private int id;
@@ -19,5 +24,86 @@ public class Hierarchy {
 
     public String getName() {
         return name;
-    }    
+    }
+
+    public ArrayList<Hierarchy> getAllHierarchies() {
+        ArrayList<Hierarchy> hierarchyList = new ArrayList<>();
+
+        String query = "SELECT * FROM jerarquias ORDER BY id_jerarquia ASC;";
+
+        try {
+            DBConnection dbConnection = new DBConnection();
+            dbConnection.connect();
+
+            ResultSet resultSet = dbConnection.executeQuery(query);
+
+            while(resultSet.next()) {
+                Hierarchy hierarchy = new Hierarchy();
+                hierarchy.setId(resultSet.getInt("id_jerarquia"));
+                hierarchy.setName(resultSet.getString("nombre"));
+
+                hierarchyList.add(hierarchy);
+            }
+
+            dbConnection.disconnect();
+        }
+        catch (Exception e) {
+        }
+
+        return hierarchyList;
+    }
+
+    public boolean addHierarchy() {
+        boolean result = false;
+        String command = "INSERT INTO jerarquias (nombre) VALUES ('" + this.name + "');";
+
+        try {
+            DBConnection dbConnection = new DBConnection();
+            dbConnection.connect();
+
+            result = dbConnection.executeCommand(command);
+
+            dbConnection.disconnect();
+        }
+        catch(Exception e) {
+        }
+
+        return result;
+    }
+
+    public boolean updateHierarchy() {
+        boolean result = false;
+        String command = "UPDATE jerarquias SET nombre = '" + this.name + "' WHERE id_jerarquia = " + this.id + ";";
+
+        try {
+            DBConnection dbConnection = new DBConnection();
+            dbConnection.connect();
+
+            result = dbConnection.executeCommand(command);
+
+            dbConnection.disconnect();
+        }
+        catch (Exception e) {
+        }
+
+        return result;
+    }
+
+    public boolean deleteHierarchy() {
+        boolean result = false;
+        String command = "DELETE FROM jerarquias WHERE id_jerarquia = " + this.id + ";";
+
+        try {
+            DBConnection dbConnection = new DBConnection();
+            dbConnection.connect();
+
+            result = dbConnection.executeCommand(command);
+
+            dbConnection.disconnect();
+        }
+        catch (Exception e) {
+        }
+
+        return result;
+    }
 }
