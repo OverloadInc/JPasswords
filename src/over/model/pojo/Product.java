ab7fb6c213790d49e3a517421c73b2ae967e4598
Juan-Alberto Hernández-Martínez <juan.alberto.hernandez.martinez@gmail.com>
2021-09-08 14:16:08 -0500

    Product class methods to access the Database added

--- src/over/model/pojo/Product.java
+++ src/over/model/pojo/Product.java
@@ -1,7 +1,11 @@
 package over.model.pojo;
 
-public class Product {
+import over.model.sql.db.DBConnection;
 
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public class Product {
     private int id;
     private String name;
     private KindOfProduct kindOfProduct;
@@ -28,5 +32,90 @@ public class Product {
 
     public KindOfProduct getKindOfProduct() {
         return kindOfProduct;
+    }
+
+    public ArrayList<Product> getAllProducts() {
+        ArrayList<Product> productList = new ArrayList<>();
+        ArrayList<KindOfProduct> kindOfProducts = new KindOfProduct().getAllKindOfProduct();
+
+        String query = "SELECT * FROM productos ORDER BY id_producto ASC;";
+
+        try {
+            DBConnection dbConnection = new DBConnection();
+            dbConnection.connect();
+
+            ResultSet resultSet = dbConnection.executeQuery(query);
+
+            while(resultSet.next()) {
+                Product product = new Product();
+
+                product.setId(resultSet.getInt("id_producto"));
+                product.setName(resultSet.getString("nombre"));
+                product.setKindOfProduct(new KindOfProduct().getKindOfProduct(kindOfProducts, resultSet.getInt("id_tipoproducto")));
+
+                productList.add(product);
+            }
+
+            dbConnection.disconnect();
+        }
+        catch (Exception e) {
+        }
+
+        return productList;
+    }
+
+    public boolean addProduct() {
+        boolean result = false;
+        String command = "INSERT INTO productos (nombre, id_tipoproducto) VALUES ('" + this.name + "', '" + this.kindOfProduct.getId() + "');";
+
+        try {
+            DBConnection dbConnection = new DBConnection();
+            dbConnection.connect();
+
+            result = dbConnection.executeCommand(command);
+
+            dbConnection.disconnect();
+        }
+        catch(Exception e) {
+        }
+
+        return result;
+    }
+
+    public boolean updateProduct() {
+        boolean result = false;
+        String command = "UPDATE productos SET nombre = '" + this.name + "', id_tipoproducto = " + this.kindOfProduct.getId() + " WHERE id_producto = " + this.id + ";";
+        System.out.println(command);
+
+        try {
+            DBConnection dbConnection = new DBConnection();
+            dbConnection.connect();
+
+            result = dbConnection.executeCommand(command);
+
+            dbConnection.disconnect();
+        }
+        catch (Exception e) {
+        }
+
+        return result;
+    }
+
+    public boolean deleteProduct() {
+        boolean result = false;
+        String command = "DELETE FROM productos WHERE id_producto = " + this.id + ";";
+
+        try {
+            DBConnection dbConnection = new DBConnection();
+            dbConnection.connect();
+
+            result = dbConnection.executeCommand(command);
+
+            dbConnection.disconnect();
+        }
+        catch (Exception e) {
+        }
+
+        return result;
     }
 }
