8c631de5a4edd1b21c58cf922b6a89da2a4ce729
Juan-Alberto Hernández-Martínez <juan.alberto.hernandez.martinez@gmail.com>
2021-09-15 18:46:19 -0500

    Class added to control the user positions

---
+++ src/over/controller/PositionsController.java
@@ -0,0 +1,75 @@
+package over.controller;
+
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.util.List;
+
+public class PositionsController implements Controller {
+    private JTextField txtPositionName;
+    private JComboBox<String> cmbDepartments;
+    private JComboBox<String> cmbHierarchies;
+    private JButton btnAddPosition;
+    private JTable existingPositionsTable;
+
+    public PositionsController(List components) {
+        txtPositionName = (JTextField) components.get(0);
+        cmbDepartments = (JComboBox) components.get(1);
+        cmbHierarchies = (JComboBox) components.get(2);
+        btnAddPosition = (JButton) components.get(3);
+        existingPositionsTable = (JTable) components.get(4);
+    }
+
+    public void addPosition() {
+
+    }
+
+    public void updatePosition() {
+
+    }
+
+    public void deletePosition() {
+
+    }
+
+    public void refreshExistingPositionsTable() {
+
+    }
+
+    @Override
+    public void selectText(String name) {
+        switch(name) {
+            case "txtPositionName" :
+                txtPositionName.setSelectionStart(0);
+                txtPositionName.setSelectionEnd(txtPositionName.getText().length());
+                break;
+        }
+    }
+
+    @Override
+    public void setFocus(KeyEvent evt) {
+        String keySource = getKeySource(evt);
+        int keyCode = evt.getKeyCode();
+
+        if(keyCode == 37 || keyCode == 39)
+            switch(keySource){
+                case "txtPositionName":
+                    btnAddPosition.requestFocusInWindow();
+                    break;
+                case "btnAddPosition":
+                    txtPositionName.requestFocusInWindow();
+                    selectText("txtPositionName");
+                    break;
+
+            }
+        else if(keyCode == 10) {
+            switch(keySource){
+                case "btnAddPosition":
+                    addPosition();
+                    refreshExistingPositionsTable();
+                    txtPositionName.setText("");
+                    txtPositionName.requestFocusInWindow();
+                    break;
+            }
+        }
+    }
+}
