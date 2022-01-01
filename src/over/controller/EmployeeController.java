19229ba4a7184c456daf3f6aaa54edd94168f8d6
Juan-Alberto Hernández-Martínez <juan.alberto.hernandez.martinez@gmail.com>
2021-12-31 18:52:15 -0600

    Add methods to set the Employees panel information

--- src/over/controller/EmployeeController.java
+++ src/over/controller/EmployeeController.java
@@ -1,17 +1,18 @@
 package over.controller;
 
 import over.model.pojo.Employee;
+import over.model.pojo.Position;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.text.DefaultFormatterFactory;
 import javax.swing.text.MaskFormatter;
 import java.awt.event.KeyEvent;
+import java.sql.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.ZoneId;
-import java.util.Date;
 import java.util.List;
 
 public class EmployeeController implements Controller{
@@ -19,7 +20,7 @@ public class EmployeeController implements Controller{
     private JTextField txtLastNameF;
     private JTextField txtLastNameM;
     private JTextField txtHeading;
-    private JTextField txtPosition;
+    private JComboBox cmbPosition;
     private JTextField txtAddress;
     private JFormattedTextField txtBirthday;
     private JFormattedTextField txtRegisterDate;
@@ -32,7 +33,7 @@ public class EmployeeController implements Controller{
         txtLastNameF = (JTextField) list.get(1);
         txtLastNameM = (JTextField) list.get(2);
         txtHeading = (JTextField) list.get(3);
-        txtPosition = (JTextField) list.get(4);
+        cmbPosition = (JComboBox) list.get(4);
         txtAddress = (JTextField) list.get(5);
         txtBirthday = (JFormattedTextField) list.get(6);
         txtRegisterDate = (JFormattedTextField) list.get(7);
@@ -42,15 +43,110 @@ public class EmployeeController implements Controller{
     }
 
     public void addEmployee() {
+        try {
+            String name = txtEmployee.getText().trim();
+            String lastNameF = txtLastNameF.getText().trim();
+            String lastNameM = txtLastNameM.getText().trim();
+            String heading = txtHeading.getText().trim();
+            Position position = (Position) cmbPosition.getSelectedItem();
+            String address = txtAddress.getText().trim();
+            String birthday = txtBirthday.getValue().toString().trim();
+            String register = txtRegisterDate.getValue().toString().trim();
+            String gender = cmbGender.getSelectedItem().toString();
 
+            SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+            java.util.Date parsed = format.parse(birthday);
+            Date birthdayDate = new Date(parsed.getTime());
+
+            java.util.Date parsed2 = format.parse(register);
+            Date registerDate = new Date(parsed2.getTime());
+
+            if(!name.isEmpty() && !lastNameF.isEmpty() && !lastNameM.isEmpty()) {
+                Employee employee = new Employee();
+
+                employee.setName(name);
+                employee.setLastNameF(lastNameF);
+                employee.setLastNameM(lastNameM);
+                employee.setTitle(heading);
+                employee.setPosition(position);
+                employee.setAddress(address);
+                employee.setBirthdate(birthdayDate);
+                employee.setRegister(registerDate);
+                employee.setGender(gender.charAt(0));
+
+                employee.addEmployee();
+            }
+            else
+                JOptionPane.showMessageDialog(null, "Introduce un nombre válido");
+        }
+        catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 
     public void deleteEmployee() {
+        int row = existingEmployeesTable.getSelectedRow();
 
+        if(row >= 0) {
+            int id = (int)existingEmployeesTable.getValueAt(existingEmployeesTable.getSelectedRow(), 0);
+
+            Employee employee = new Employee();
+            employee.setId(id);
+
+            employee.deleteEmployee();
+        }
+        else
+            JOptionPane.showMessageDialog(null, "Selecciona un empleado");
     }
 
     public void updateEmployee() {
+        int row = existingEmployeesTable.getSelectedRow();
 
+        if(row >= 0) {
+            try {
+                int id = Integer.parseInt(existingEmployeesTable.getValueAt(row, 0).toString());
+                String name = txtEmployee.getText().trim();
+                String lastNameF = txtLastNameF.getText().trim();
+                String lastNameM = txtLastNameM.getText().trim();
+                String heading = txtHeading.getText().trim();
+                Position position = (Position) cmbPosition.getSelectedItem();
+                String address = txtAddress.getText().trim();
+                String birthday = txtBirthday.getValue().toString().trim();
+                String register = txtRegisterDate.getValue().toString().trim();
+                String gender = cmbGender.getSelectedItem().toString();
+
+                SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+                java.util.Date parsed = format.parse(birthday);
+                Date birthdayDate = new Date(parsed.getTime());
+
+                java.util.Date parsed2 = format.parse(register);
+                Date registerDate = new Date(parsed2.getTime());
+
+                if(!name.isEmpty() && !lastNameF.isEmpty() && !lastNameM.isEmpty()) {
+                    Employee employee = new Employee();
+
+                    employee.setId(id);
+                    employee.setName(name);
+                    employee.setLastNameF(lastNameF);
+                    employee.setLastNameM(lastNameM);
+                    employee.setTitle(heading);
+                    employee.setPosition(position);
+                    employee.setAddress(address);
+                    employee.setBirthdate(birthdayDate);
+                    employee.setRegister(registerDate);
+                    employee.setGender(gender.charAt(0));
+
+                    employee.updateEmployee();
+                }
+                else
+                    JOptionPane.showMessageDialog(null, "Introduce un nombre válido");
+            }
+            catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
+        else
+            JOptionPane.showMessageDialog(null, "Selecciona un empleado");
     }
 
     public void refreshExistingEmployeeTable() {
@@ -77,6 +173,13 @@ public class EmployeeController implements Controller{
         }
     }
 
+    public void refreshExistingPositionsList() {
+        cmbPosition.removeAllItems();
+
+        for (Position position : new Position().getAllpositions())
+            cmbPosition.addItem(position);
+    }
+
     public void refreshExistingGenderList() {
         cmbGender.removeAllItems();
 
@@ -97,20 +200,20 @@ public class EmployeeController implements Controller{
         String lastNameF = existingEmployeesTable.getValueAt(existingEmployeesTable.getSelectedRow(), 2).toString();
         String lastNameM = existingEmployeesTable.getValueAt(existingEmployeesTable.getSelectedRow(), 3).toString();
         String title = existingEmployeesTable.getValueAt(existingEmployeesTable.getSelectedRow(), 4).toString();
-        String position = existingEmployeesTable.getValueAt(existingEmployeesTable.getSelectedRow(), 5).toString();
+        Position position = (Position)existingEmployeesTable.getValueAt(existingEmployeesTable.getSelectedRow(), 5);
         String address = existingEmployeesTable.getValueAt(existingEmployeesTable.getSelectedRow(), 6).toString();
         String birthday = existingEmployeesTable.getValueAt(existingEmployeesTable.getSelectedRow(), 7).toString();
         String register = existingEmployeesTable.getValueAt(existingEmployeesTable.getSelectedRow(), 8).toString();
         String gender = existingEmployeesTable.getValueAt(existingEmployeesTable.getSelectedRow(), 9).toString();
 
-        Date birthdayDate = Date.from(LocalDate.parse(birthday).atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
-        Date registerDate = Date.from(LocalDate.parse(register).atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
+        java.util.Date birthdayDate = Date.from(LocalDate.parse(birthday).atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
+        java.util.Date registerDate = Date.from(LocalDate.parse(register).atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
 
         txtEmployee.setText(name);
         txtLastNameF.setText(lastNameF);
         txtLastNameM.setText(lastNameM);
         txtHeading.setText(title);
-        txtPosition.setText(position);
+        cmbPosition.setSelectedIndex(position.getId() - 1);
         txtAddress.setText(address);
         txtBirthday.setValue(simpleDateFormat.format(birthdayDate));
         txtRegisterDate.setValue(simpleDateFormat.format(registerDate));
@@ -152,22 +255,26 @@ public class EmployeeController implements Controller{
             switch(keySource){
                 case "btnAddEmployee":
                     addEmployee();
-                    refreshExistingGenderList();
-                    refreshExistingEmployeeTable();
                     break;
                 case "btnModifyEmployee":
                     updateEmployee();
-                    refreshExistingGenderList();
-                    refreshExistingEmployeeTable();
                     break;
                 case "btnDeleteEmployee":
                     deleteEmployee();
-                    refreshExistingGenderList();
-                    refreshExistingEmployeeTable();
                     break;
             }
 
+            refreshExistingPositionsList();
+            refreshExistingGenderList();
+            refreshExistingEmployeeTable();
+
             txtEmployee.setText("");
+            txtLastNameF.setText("");
+            txtLastNameM.setText("");
+            txtHeading.setText("");
+            txtAddress.setText("");
+            txtBirthday.setText("");
+            txtRegisterDate.setText("");
             txtEmployee.requestFocusInWindow();
         }
     }
